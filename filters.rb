f0b2b9ea886354ee3cea1af15b0100e6f3e78bbc
Aiman03at <aiman03at@gmail.com>
2024-10-12 22:15:04 -0400

    Add all the filter methods

--- filters.rb
+++ filters.rb
@@ -1,16 +1,39 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
+
 def find(id)
     # Your code Here
+    candidates.each do |candidate|
+      return candidate if candidate[:id] == id
+    end
+    
+    nil  # Return nil if no candidate is found
+  
   end
   
   def experienced?(candidate)
     # Your code Here
+    candidate[:years_of_experience] >= 2
   end
   
   def qualified_candidates(candidates)
-    # Your code Here
+    candidates.select do |candidate|
+      experienced?(candidate) &&
+        has_github_points?(candidate, 100) &&
+        knows_language?(candidate, ["Ruby", "Python"]) &&
+        applied_recently?(candidate, 15) &&
+        over_age?(candidate, 17)
+    end
+  
   end
   
   # More methods will go below
+
+
+  def ordered_by_qualifications(candidates)
+    candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+  end
+
+
+  
--- main.rb
+++ main.rb
@@ -4,6 +4,14 @@ require 'pp'
 require './candidates'
 require './filters'
 
-## Your test code can go here
 
-pp @candidates
+candidates = @candidates
+
+# Call the ordered_by_qualifications method
+ordered_candidates = ordered_by_qualifications(candidates)
+
+# Pretty print the ordered candidates
+pp ordered_candidates
+# Pretty print the original candidates (if you want to see both)
+
+
